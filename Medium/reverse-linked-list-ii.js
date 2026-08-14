cc4327ca3ad1a8ac16264b768090f79e630887ee
itzmeranjeet <134777442+itzmeranjeet@users.noreply.github.com>
2026-08-14 23:33:27 +0530

    Add Medium/reverse-linked-list-ii.js

---
+++ Medium/reverse-linked-list-ii.js
@@ -0,0 +1,25 @@
+/*
+ * LeetCode: Reverse Linked List II
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/reverse-linked-list-ii/description/
+ * Language: javascript
+ *
+ * Synced by Personal LeetHub
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    
+};
