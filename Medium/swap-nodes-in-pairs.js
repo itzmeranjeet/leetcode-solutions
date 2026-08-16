64c887b04326e45f3bfb9522618efbbf65a905bf
itzmeranjeet <134777442+itzmeranjeet@users.noreply.github.com>
2026-08-16 23:20:42 +0530

    Add Medium/swap-nodes-in-pairs.js

---
+++ Medium/swap-nodes-in-pairs.js
@@ -0,0 +1,51 @@
+/*
+ * LeetCode: Swap Nodes in Pairs
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/swap-nodes-in-pairs/
+ * Language: javascript
+ *
+ * Synced by Personal LeetHub
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(head === null || head.next ===null){
+        return head
+    }
+  let first = head
+  let second = head.next
+  let prev = null
+
+  while(first !== null && second !== null){
+    let third = second.next
+    second.next = first
+    first.next = third
+
+    if(prev !== null){
+        prev.next = second
+    }else{
+        head = second
+    }
+
+    prev = fisrt
+    fisrt = third
+
+    if(third !== null){
+        second = third.next
+    }else{
+        second = null
+    }
+  }
+
+  return head
+};
